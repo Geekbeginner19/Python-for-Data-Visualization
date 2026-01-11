e0b864c951cfb69a10d8591674de42c204d37657
Kelvin_Ofori_Amoafo <86635380+Geekbeginner19@users.noreply.github.com>
2026-01-11 10:12:52 +0000

    Weekly Phone Usage Using Pandas & Plotly & Plotly

---
+++ Weekly Phone Usage Using Pandas.py
@@ -0,0 +1,35 @@
+# 🛠 MINI PROJECT 2
+# 🎯 Objective
+# Practice plotting from Pandas.
+
+# 🧩 Task
+# Create a DataFrame showing your weekly expenses.
+
+# Example columns:
+# Day
+# Amount_Spent
+
+# Requirements
+# ✔ Use Pandas
+# ✔ Use plotly.express
+# ✔ Use a bar chart
+# ✔ Add a title
+
+import plotly.express as px #For plotting information
+import pandas as pd #For processing data
+
+#Creating a DataFrame to process the original data (Original Data HAS to be converted to a dictionary)
+df = pd.DataFrame({'Days' : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+'Hours_spent' : [5, 7, 3, 8, 5, 1, 6]
+})
+#Plotting the data
+figure = px.line(df, x = 'Days', y = 'Hours_spent', title = 'Weekly Phone Usage (Using Pandas)', color_discrete_sequence = ["royalblue"], template = "plotly_dark")
+
+#Adding Markers to Lines (Plotly expects NO SPACES in the mode string) 
+figure.update_traces(mode = "lines+markers")
+
+#Labelling Axes & Changing Figure Size
+figure.update_layout(xaxis_title = "Days of the Week", yaxis_title = "Hours Spent on Phone", width = 900, height = 450)
+
+#Displaying the graph
+figure.show()
